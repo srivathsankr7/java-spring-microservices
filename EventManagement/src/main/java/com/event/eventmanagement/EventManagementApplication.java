09668aa41cd5e58a60480ec0c4dfa8905fac8e76
srivathsankr7 <srivathsankr7@gmail.com>
2020-02-10 21:21:25 +0530

    Develop event CRUD services

--- EventManagement/src/main/java/com/event/eventmanagement/EventManagementApplication.java
+++ EventManagement/src/main/java/com/event/eventmanagement/EventManagementApplication.java
@@ -2,8 +2,10 @@ package com.event.eventmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EventManagementApplication {
 
 	public static void main(String[] args) {
